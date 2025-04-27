19268741e999c21733d640c9bab0b48afb3e0775
Eljan Najafov <eljan.najafov02@mail.ru>
2025-04-27 08:39:11 +0400

    sec conf

--- src/main/java/com/greenpulse/auth/user_management_service/config/SecurityConfig.java
+++ src/main/java/com/greenpulse/auth/user_management_service/config/SecurityConfig.java
@@ -1,21 +1,13 @@
 package com.greenpulse.auth.user_management_service.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
